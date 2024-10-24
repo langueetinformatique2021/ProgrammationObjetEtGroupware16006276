4f682e27479bd2693eedab8b33449a0f150556fa
frede <frede@10.51.54.127>
2024-10-24 10:11:36 +0200

    ex1.1

---
+++ TD/atelier04/Condition1.java
@@ -0,0 +1,10 @@
+package atelier04;
+
+public class Condition1 {
+	public static void main(String[] args) {
+	int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+	
+	
+	}
+}
+
---
+++ TD/atelier04/TD4Ex1.java
@@ -0,0 +1,25 @@
+package atelier04;
+
+public class Condition1 {
+	public static void main(String[] args) {
+	int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+	
+	int max=p1;
+	
+	if (p2>max) {
+		max = p2n;
+	}
+	if (p3>max) {
+		max=p3;
+	}
+	if (p4>max) {
+		max = p4:
+	}
+	if (p5>maxP ) {
+		max = p5:
+	}
+	
+	System.out.println("Le maximum est: " + max);
+	}
+}
+			
